3b6c3bd7a20059744ce9722d1ddccc819a378c7b
Eric Q <eric.qiu2006@gmail.com>
2025-08-24 13:43:45 -0700

    fixing customers blank page issue.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
+// /app/dashboard/customers/page.tsx
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
